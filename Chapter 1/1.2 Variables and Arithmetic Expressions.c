e7d8a532e0981c0dd7641fa31bf7c16c4146394c
Talkasi <kitalkasi@gmail.com>
2022-02-03 23:04:12 +0300

    Trying to solve merge conflict 2.0

--- Chapter 1/1.2 Variables and Arithmetic Expressions.c
+++ Chapter 1/1.2 Variables and Arithmetic Expressions.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/* Definition */
+#define LOWER_T 0
+#define UPPER_T 300
+#define STEP 20
+
 /* Print Fahrenheit-Celsius table */
 
 int main() {
@@ -7,7 +12,7 @@ int main() {
     printf("Fahrenheit-Celsius table:\n");
     /* Initialisation */
     int fahr, celsius;
-    int lower = 0, upper = 300, step = 20;
+    int lower = LOWER_T, upper = UPPER_T;
 
     fahr = lower;
 
@@ -18,15 +23,15 @@ int main() {
          */
         celsius = (fahr - 32) * 5 / 9;
         printf("%3d %6d\n", fahr, celsius);
-        fahr += step;
+        fahr += STEP;
     }
     /* Part 2 */
     printf("\nThe second variant of this task solution:\n");
-    float celsius_2, fahr_2 = 0;
+    float celsius_2, fahr_2 = LOWER_T;
 
     while (fahr_2 <= upper) {
         celsius_2 = (5.0 / 9.0) * (fahr_2 - 32.0);
         printf("%3.0f %6.1f\n", fahr_2, celsius_2);
-        fahr_2 += 20;
+        fahr_2 += STEP;
     }
 };
