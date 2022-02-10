e2d5c149aeea4218e64c4c5cf9bd22ff8f58cd94
Talkasi <kitalkasi@gmail.com>
2022-02-10 23:36:37 +0300

    Adding code to understand things better

---
+++ Chapter 1/1.10 External variables and scope.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+#define MAX_LINE 1000
+
+int max;
+char line[MAX_LINE];
+char longest_line[MAX_LINE];
+
+int getline(void);
+
+void copy(void);
+
+char *reversed(char line[], int len);
+
+/* TODO:  - fix bug;
+ *        - try to add reversed here */
+
+int main() {
+    int len;
+    extern int max;
+    extern char longest_line[MAX_LINE];
+
+    max = 0;
+    while ((len = getline()) > 0) {
+        printf("We have %d symb in this line.\n", len);
+        if (len > max) {
+            max = len;
+            copy();
+        }
+    }
+    if (max > 0) {
+        printf("Longest line is %s\n", longest_line);
+        // printf("Reversed line is %s", reversed(longest_line, max));
+    }
+}
+/*
+char *reversed(char line[], int len) {
+    int i;
+    char s[len];
+
+    for (i = 0; i < len; ++i) {
+        s[i] = line[len - i - 1];
+    }
+
+    copy();
+    if (line[len - 1] == '\n' || line[len - 1] == '\0' || line[len - 1] == '\7') {
+        line[len - 1] = '\0';
+    }
+    return line;
+}
+*/
+int getline(void) {
+    int c, i;
+    extern char line[];
+
+    for (i = 0; i < MAX_LINE - 1 && (c = getchar()) != '.'; ++i) {
+        if (c != '\n') {
+            line[i] = c;
+        }
+    }
+
+
+    line[i] = '\0';
+    return i;
+}
+
+void copy(void) {
+    int i = 0;
+    extern char line[], longest_line[];
+
+    while ((longest_line[i] = line[i]) != '\0') {
+        ++i;
+    }
+}
