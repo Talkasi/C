3f724a2dee2c57defd37087c907f294762ea2411
TalKasi <95167715+Talkasi@users.noreply.github.com>
2022-03-12 17:32:48 +0300

    Update Twice as old.c

--- Codewars stuff/Twice as old.c
+++ Codewars stuff/Twice as old.c
@@ -1,5 +1,5 @@
-#include <math.h>
 #include <stdio.h>
+#include <math.h>
 
 int twice_as_old_v1(int father_age, int son_age) {
     int n_years = 0;
@@ -21,7 +21,7 @@ int twice_as_old_v1(int father_age, int son_age) {
     return n_years;
 }
 
-/* While the solution consist from 1 line)) */
+/* While the solution consist from 1 line */
 int twice_as_old_v2(int father_age, int son_age) {
     return abs(father_age - son_age * 2);
 }
@@ -29,5 +29,5 @@ int twice_as_old_v2(int father_age, int son_age) {
 int main() {
     int test_value_1 = 29, test_value_2 = 0;
     printf("V1: %d\n", twice_as_old_v1(test_value_1, test_value_2));
-    printf("V2: %d", twice_as_old_v2(test_value_1, test_value_2));
+    printf("V2: %d",   twice_as_old_v2(test_value_1, test_value_2));
 }
