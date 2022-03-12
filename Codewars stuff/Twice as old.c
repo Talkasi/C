d09ddc9045cf4ded85e87e1843d631c5a358dc90
Talkasi <kitalkasi@gmail.com>
2022-03-12 09:51:08 +0300

    Twice as old sollution

---
+++ Codewars stuff/Twice as old.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int twice_as_old_v1(int father_age, int son_age)
+{
+    int n_years = 0;
+
+    if (father_age < son_age * 2) {
+        while (father_age != son_age * 2) {
+            ++n_years;
+            --father_age;
+            --son_age;
+        }
+        return n_years;
+    } else {
+        if (father_age != son_age * 2) {
+            while (father_age != son_age * 2) {
+                ++n_years;
+                ++father_age;
+                ++son_age;
+            }
+        }
+    }
+    return n_years;
+}
+
+
+/* While the solution consist of 1 line :) */
+int twice_as_old_v2(int father_age, int son_age)
+{
+    return abs(father_age - son_age * 2);
+}
+
+
+int main()
+{
+    int test_value_1 = 29, test_value_2 = 0;
+    printf("V1: %d\n", twice_as_old_v1(test_value_1, test_value_2));
+    printf("V2: %d", twice_as_old_v2(test_value_1, test_value_2));
+}
