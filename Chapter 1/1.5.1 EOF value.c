d9988ce2c7cad80e2e72ac04ac54fc71b2e7a83e
Talkasi <kitalkasi@gmail.com>
2023-02-11 17:13:43 +0300

    Tasks for 1.5.3 Line counting block added + stuff

---
+++ Chapter 1/1.5.1 EOF value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int c;
+
+	c = 10;
+	printf("%d\n", EOF);
+	printf("%d\n", c);
+}
+
---
+++ Chapter 1/1.5.3 Blanks to blank.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* Why does `putchar()` work only when input stream for `getchar()` is over? */
+
+/* This misunderstanding appears, because terminal is line-buffered. */
+/* Functions still only work on single characters but the terminal waits */
+/* with submitting the characters to the program until the whole line wasn't entered */
+
+int main() {
+	long c, prev_c = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (prev_c == ' ' && c == ' ')
+			;
+		else 
+			putchar(c);
+		prev_c = c;
+	}
+}
+
